496447bab322a9d69acd34b6044475f3afa43d44
Gary Grumbley <ggrumbley@gmail.com>
2022-04-29 13:49:38 -0700

    feat(part-3): add arguments work

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -1,14 +1,17 @@
 const resolvers = {
   Query: {
     // returns an array of Tracks that will be used to populate the homepage grid of our web client
-    tracksForHome: (_, __, { dataSources }) => {
-      return dataSources.trackAPI.getTracksForHome();
-    },
+    tracksForHome: (_, __, { dataSources }) =>
+      dataSources.trackAPI.getTracksForHome(),
+
+    track: (_, { id }, { dataSources }) => dataSources.trackAPI.getTrack(id),
   },
   Track: {
-    author: ({ authorId }, _, { dataSources }) => {
-      return dataSources.trackAPI.getAuthor(authorId);
-    },
+    author: ({ authorId }, _, { dataSources }) =>
+      dataSources.trackAPI.getAuthor(authorId),
+
+    modules: ({ id }, _, { dataSources }) =>
+      dataSources.trackAPI.getTrackModules(id),
   },
 };
 
